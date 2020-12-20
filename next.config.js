76b7bf6786513460ffde65e92c195c1d1bc50b00
olvlesya <lesyonoknou@gmail.com>
2020-12-20 22:30:56 +0100

    fix: add basePath

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
+  basePath: isProd ? "/internet-shop/out" : "",
   assetPrefix: isProd ? "https://olvlesya.github.io/internet-shop/out/" : "",
 };
