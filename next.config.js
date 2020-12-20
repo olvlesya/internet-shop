9588164de9b4ed83388a9edc651f93464d0aca0c
olvlesya <lesyonoknou@gmail.com>
2020-12-20 22:39:07 +0100

    fix: use statically for links

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  basePath: isProd ? "/internet-shop/out" : "",
-  assetPrefix: isProd ? "https://olvlesya.github.io/internet-shop/out/" : "",
+  assetPrefix: isProd
+    ? "https://cdn.statically.io/gh/olvlesya/internet-shop/gh-pages/out/"
+    : "",
 };
