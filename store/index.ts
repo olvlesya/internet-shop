07ac4bb3443dfa2fb2c219daccf4a0d47f5fb4e8
olvlesya <lesyonoknou@gmail.com>
2021-01-16 18:09:13 +0100

    feat: add async actions

--- store/index.ts
+++ store/index.ts
@@ -8,3 +8,5 @@ export { itemsInit } from "./items/actions";
 export { startLoading, stopLoading } from "./loadingState/actions";
 export { sortAsc, sortDesc } from "./sort/actions";
 export { searchItems } from "./searchQuery/actions";
+
+export { loadItems } from "./asyncActions/loadItems";
